a3b65fb31537b6c0a7a6fd7a1235066f4157aca1
jsm512 <wlstmdals12@naver.com>
2024-10-12 15:50:14 +0900

    init

---
+++ src/main/java/com/ureca/githook/GithookApplication.java
@@ -0,0 +1,13 @@
+package com.ureca.githook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ureca/githook/GithookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.githook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
